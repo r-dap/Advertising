8e3a23a843cb54f0a1d62f31f2102fe7254eb133
unknown <a8861501@gmail.com>
2023-05-02 21:55:02 +0800

    add comments

--- upload/urls.py
+++ upload/urls.py
@@ -6,10 +6,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('upload/', views.upload_file,name='upload file'),
-    path('upload/play/',views.play),
-    path('delete/',views.delete,name='delete'),
-    path('media/<path:file_path>/',views.show_file,name='show file'),
-    path('controlplayer/',views.control_player,name='control player')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('',views.index,name='index'), 
+    path('upload/', views.upload_file,name='upload file'), # 上傳文件頁面
+    path('upload/play/',views.play), # `播放頁面
+    path('delete/',views.delete,name='delete'), # `刪除按鈕
+    path('media/<path:file_path>/',views.show_file,name='show file'), # 播放影片
+    path('controlplayer/',views.control_player,name='control player') # 控制播放器，暫停 撥放 停止
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) # 靜態文件路徑，用來讀static資料夾的檔案
